376fcae7b41d3c2877b1ac466c2246c5206e6f71
ngan <vutran@sheridancollege.ca>
2025-10-12 22:20:26 -0400

    Routes Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes/AppRoutes";
+import { NavBar } from "./components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl underline font-dm-sans">Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
