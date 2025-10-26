39bc400b40e78befc7a0b90c1be4695b4d20f743
ngan <vutran@sheridancollege.ca>
2025-10-25 21:54:19 -0400

    Update styles for desktop version

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -11,26 +11,34 @@ export function SearchForm() {
     data && setAllCities(getAllCities(data));
   }, [data]);
   return (
-    <div className="mt-[20px] px-[40px] flex flex-col gap-y-[10px]">
-      <div className="relative flex items-center gap-x-[10px] bg-n-700 rounded-xl p-[10px]">
-        <MagnifyingGlassIcon className="w-[27px] h-full" />
-        <input
-          type="text"
-          name="location"
-          id="location"
-          placeholder="Search for a place..."
-          className="flex-1 outline-none"
-          onChange={(e) => setLocationInput(e.target.value)}
-        />
-        {/* <ul className="absolute left-0 right-0 top-[110%] bg-n-700 rounded-xl p-[10px]">
+    <div className="flex items-center justify-center px-[40px] mt-[30px]">
+      <div className="flex flex-col md:flex-row gap-x-4 gap-y-[10px] w-full max-w-[600px]">
+        <div className="relative md:flex-1 flex items-center gap-x-[10px] bg-n-700 rounded-xl p-[10px]">
+          <MagnifyingGlassIcon className="w-[27px] h-full" />
+          <input
+            type="text"
+            name="location"
+            id="location"
+            placeholder="Search for a place..."
+            className="flex-1 outline-none"
+            onChange={(e) => setLocationInput(e.target.value)}
+          />
+          {/* <ul className="absolute left-0 right-0 top-[110%] bg-n-700 rounded-xl p-[10px]">
+          <li className="hover:bg-n-600 rounded-xl p-[10px] mb-[10px] cursor-pointer">
+            <button>Test</button>
+          </li>
+          <li className="hover:bg-n-600 rounded-xl p-[10px] mb-[10px] cursor-pointer">
+            <button>Test</button>
+          </li>
           <li className="hover:bg-n-600 rounded-xl p-[10px] mb-[10px] cursor-pointer">
             <button>Test</button>
           </li>
         </ul> */}
+        </div>
+        <button className="p-[10px] md:px-[20px] bg-blue-custom-500 hover:bg-blue-custom-700 text-n-0 rounded-xl cursor-pointer duration-200 active:scale-95">
+          Search
+        </button>
       </div>
-      <button className="p-[10px] bg-blue-custom-500 hover:bg-blue-custom-700 text-n-0 rounded-xl cursor-pointer duration-200 active:scale-95">
-        Search
-      </button>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { SearchForm } from "../components/SearchForm";
 export function Home() {
   return (
     <div className="font-dm-sans mt-[30px]">
-      <h1 className="text-3xl text-center text-n-0 font-bold">
+      <h1 className="text-3xl md:text-5xl text-center text-n-0 font-bold">
         How's the sky looking today?
       </h1>
       <SearchForm />
