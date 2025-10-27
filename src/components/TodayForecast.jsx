82940a8c2b80157df8b9933314924c622636785b
ngan <vutran@sheridancollege.ca>
2025-10-26 21:23:10 -0400

    The background image for the first tile of the today's forecast is finished

--- src/components/TodayForecast.jsx
+++ src/components/TodayForecast.jsx
@@ -5,12 +5,13 @@ export function TodayForecast() {
   return (
     <div className="px-[40px] mt-[20px]">
       <div className="grid gap-4 grid-cols-2">
-        <div className="relative w-full p-[10px] bg-n-700 rounded-xl col-span-2 min-h-[200px]">
-          <img
-            alt="bgTodaySmall"
-            srcset={bgTodaySmall}
-            className="absolute inset-0 rounded-xl"
-          />
+        <div
+          className="w-full p-[10px] bg-n-700 rounded-xl col-span-2 min-h-[200px] bg-cover bg-center bg-no-repeat"
+          style={{
+            backgroundImage: `url(${bgTodaySmall})`,
+            backgroundPositionY: "-50px",
+          }}
+        >
           <p className="font-bold text-2xl text-center mt-[15px] text-n-0">
             Berlin, Germany
           </p>
